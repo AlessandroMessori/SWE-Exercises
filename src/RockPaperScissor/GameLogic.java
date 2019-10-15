fbbbf621807abae632935ed16879578f301e5644
AlessandroMessori <messori.alessandro.98@gmail.com>
2019-10-15 16:47:48 +0200

    added scoreboard

--- src/RockPaperScissor/GameLogic.java
+++ src/RockPaperScissor/GameLogic.java
@@ -7,17 +7,24 @@ import java.util.Random;
 
 public class GameLogic {
 
+    private int winCounter;
+    private int drawCounter;
+    private int loseCounter;
+
     private final Scanner scanner;
 
     public GameLogic(InputStream input) {
         this.scanner = new Scanner(input);
+        this.winCounter = 0;
+        this.drawCounter = 0;
+        this.loseCounter = 0;
     }
 
-    public void close() {
+    private void close() {
         this.scanner.close();
     }
 
-    private Outcome choiceSubmitted(Choice userChoice) throws Exception {
+    private Outcome choiceSubmitted(Choice userChoice) {
         Random r = new Random();
         Choice computerChoice = Choice.getRandomChoice(r);
         Outcome outcome;
@@ -27,41 +34,39 @@ public class GameLogic {
         outcome = userChoice.resultAgainst(computerChoice);
         System.out.println(outcome.Message);
 
-        while (outcome == Outcome.DRAW) {
-            String move = this.scanner.nextLine();
-
-
-            if (move.equals("END")) {
-                throw new Exception();
-            }
-
-            userChoice = Choice.parseInput(move);
-            computerChoice = Choice.getRandomChoice(r);
-            System.out.println("il computer gioca " + computerChoice.toString());
-
-            outcome = userChoice.resultAgainst(computerChoice);
-            System.out.println(outcome.Message);
-
-        }
-
 
         return outcome;
     }
 
+    private void updateScoreBoard(Outcome out) {
+        switch (out) {
+            case WINNER:
+                this.winCounter++;
+                break;
+            case DRAW:
+                this.drawCounter++;
+                break;
+            case LOSER:
+                this.loseCounter++;
+                break;
+        }
+    }
 
-    public void round() {
+    private void printScoreboard() {
+        System.out.printf("Vittorie: %d, Pareggi: %d, Sconfitte %d\n", this.winCounter, this.drawCounter, this.loseCounter);
+    }
+
+
+    void round() {
         String move = this.scanner.nextLine();
         while (!move.equals("END")) {
             Choice userChoice = Choice.parseInput(move);
-            try {
-
-                Outcome outcome = this.choiceSubmitted(userChoice);
-            } catch (Exception e) {
-                break;
-            }
+            Outcome outcome = this.choiceSubmitted(userChoice);
+            this.updateScoreBoard(outcome);
+            this.printScoreboard();
             move = this.scanner.nextLine();
         }
-
+        this.close();
     }
 
 
